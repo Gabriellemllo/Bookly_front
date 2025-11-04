03ee18277cf753f273d70916fe6f493554594290
elcio <elcioferreira398@gmail.com>
2025-11-04 03:06:41 +0000

    feat: commit home construction

--- bookly/app/index.tsx
+++ bookly/app/index.tsx
@@ -3,12 +3,24 @@ import { useRouter } from 'expo-router';
 import SplashTela from './auth/splash';
 
 export default function Index() {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.replace('/auth/login');
+  //   }, 7000); 
+
+  //   return () => clearTimeout(timer);
+  // }, [router]);
+
+  // return <SplashTela />;
+
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace('/auth/login');
-    }, 7000); 
+      router.replace('/auth/(tabs)/home');
+    }, 1000); 
 
     return () => clearTimeout(timer);
   }, [router]);
