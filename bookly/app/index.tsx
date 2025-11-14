8a5f613d8487858c35896a68d193785f66ea9d38
elcio <elcioferreira398@gmail.com>
2025-11-14 01:53:02 +0000

    alterando index

--- bookly/app/index.tsx
+++ bookly/app/index.tsx
@@ -1,29 +1,20 @@
 import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
+import { useRouter, Redirect } from 'expo-router';
 import SplashTela from './auth/splash';
 
 export default function Index() {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     router.replace('/auth/login');
-  //   }, 7000); 
-
-  //   return () => clearTimeout(timer);
-  // }, [router]);
-
-  // return <SplashTela />;
-
   const router = useRouter();
 
   useEffect(() => {
+    
     const timer = setTimeout(() => {
-      router.replace('/auth/(tabs)/home');
-    }, 1000); 
+      router.replace('/auth/(tabs)/home'); 
+    }, 7000); 
 
     return () => clearTimeout(timer);
   }, [router]);
 
+
   return <SplashTela />;
+
 }
